c8e0abff14abb214b0780b2b41e09d57d7f05b0a
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-02-17 21:12:55 -0300

    migrations e models

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
@@ -12,7 +13,10 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        return "index";
+        // return "index";
+
+        $produtos = Produto::all();
+        return dd($produtos);
     }
 
     /**
